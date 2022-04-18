4094adf0d2dfd78e585dbb47f32ea92a3ba03411
AlexandrLo <alexandr.lo@outlook.com>
2022-04-18 20:33:19 +0300

    Add number input

---
+++ src/components/NumberInput/index.js
@@ -0,0 +1,48 @@
+import React from "react";
+
+import { AddFilled, SubtractFilled } from "@fluentui/react-icons";
+import {
+  Button,
+  Hide,
+  HStack,
+  Input,
+  Show,
+  useNumberInput,
+} from "@chakra-ui/react";
+
+function NumberInput({ size, ...props }) {
+  const { getInputProps, getIncrementButtonProps, getDecrementButtonProps } =
+    useNumberInput({
+      step: 1,
+      defaultValue: 1,
+      min: 0,
+      max: 99,
+      precision: 0,
+    });
+
+  const inc = getIncrementButtonProps();
+  const dec = getDecrementButtonProps();
+  const input = getInputProps();
+
+  return (
+    <HStack spacing={{ base: "0.25rem", md: "0.5rem" }}>
+      <Button variant="brand" size={size ? size : "md"} {...dec}>
+        <SubtractFilled fontSize="1.5rem" />
+      </Button>
+      <Input
+        type="number"
+        variant="brand"
+        px="0"
+        textAlign="center"
+        size={size ? size : "md"}
+        {...props}
+        {...input}
+      />
+      <Button variant="brand" size={size ? size : "md"} {...inc}>
+        <AddFilled fontSize="1.5rem" />
+      </Button>
+    </HStack>
+  );
+}
+
+export default NumberInput;
--- src/containers/TestPage/index.js
+++ src/containers/TestPage/index.js
@@ -2,6 +2,7 @@ import React from "react";
 
 import { ArrowLeftFilled } from "@fluentui/react-icons";
 import {
+  Box,
   Button,
   Container,
   HStack,
@@ -10,85 +11,99 @@ import {
   VStack,
 } from "@chakra-ui/react";
 
+import NumberInput from "components/NumberInput";
+
 function TestPage() {
   return (
     <Container>
-      <VStack align="start">
-        <Heading>Button</Heading>
-        {/* Blur buttons */}
-        <HStack>
-          <Button
-            size="md"
-            variant="blur"
-            leftIcon={<ArrowLeftFilled fontSize="1.5rem" />}
-          >
-            Back
-          </Button>
-          <Button
-            size="sm"
-            variant="blur"
-            leftIcon={<ArrowLeftFilled fontSize="1.5rem" />}
-          >
-            Back
-          </Button>
-        </HStack>
-        {/* Primary buttons */}
-        <HStack>
-          <Button
-            size="md"
-            variant="primary"
-            leftIcon={<ArrowLeftFilled fontSize="1.5rem" />}
-          >
-            Back
-          </Button>
-          <Button
-            size="sm"
-            variant="primary"
-            leftIcon={<ArrowLeftFilled fontSize="1.5rem" />}
-          >
-            Back
-          </Button>
-        </HStack>
-        {/* Secondary buttons */}
-        <HStack>
-          <Button
-            size="md"
-            variant="secondary"
-            leftIcon={<ArrowLeftFilled fontSize="1.5rem" />}
-          >
-            Back
-          </Button>
-          <Button
-            size="sm"
-            variant="secondary"
-            leftIcon={<ArrowLeftFilled fontSize="1.5rem" />}
-          >
-            Back
-          </Button>
-        </HStack>
-        {/* Brand buttons */}
-        <HStack>
-          <Button
-            size="md"
-            variant="brand"
-            leftIcon={<ArrowLeftFilled fontSize="1.5rem" />}
-          >
-            Back
-          </Button>
-          <Button
-            size="sm"
-            variant="brand"
-            leftIcon={<ArrowLeftFilled fontSize="1.5rem" />}
-          >
-            Back
-          </Button>
-        </HStack>
+      <VStack align="start" spacing="1.5rem">
+        <VStack align="start" spacing="0.5rem">
+          <Heading as="h2" size="h2">
+            Button
+          </Heading>
+          {/* Blur buttons */}
+          <HStack>
+            <Button
+              size="md"
+              variant="blur"
+              leftIcon={<ArrowLeftFilled fontSize="1.5rem" />}
+            >
+              Back
+            </Button>
+            <Button
+              size="sm"
+              variant="blur"
+              leftIcon={<ArrowLeftFilled fontSize="1.5rem" />}
+            >
+              Back
+            </Button>
+          </HStack>
+          {/* Primary buttons */}
+          <HStack>
+            <Button
+              size="md"
+              variant="primary"
+              leftIcon={<ArrowLeftFilled fontSize="1.5rem" />}
+            >
+              Back
+            </Button>
+            <Button
+              size="sm"
+              variant="primary"
+              leftIcon={<ArrowLeftFilled fontSize="1.5rem" />}
+            >
+              Back
+            </Button>
+          </HStack>
+          {/* Secondary buttons */}
+          <HStack>
+            <Button
+              size="md"
+              variant="secondary"
+              leftIcon={<ArrowLeftFilled fontSize="1.5rem" />}
+            >
+              Back
+            </Button>
+            <Button
+              size="sm"
+              variant="secondary"
+              leftIcon={<ArrowLeftFilled fontSize="1.5rem" />}
+            >
+              Back
+            </Button>
+          </HStack>
+          {/* Brand buttons */}
+          <HStack>
+            <Button
+              size="md"
+              variant="brand"
+              leftIcon={<ArrowLeftFilled fontSize="1.5rem" />}
+            >
+              Back
+            </Button>
+            <Button
+              size="sm"
+              variant="brand"
+              leftIcon={<ArrowLeftFilled fontSize="1.5rem" />}
+            >
+              Back
+            </Button>
+          </HStack>
+        </VStack>
 
-        <Heading>Input</Heading>
-        <HStack>
-          <Input placeholder="Search" size="md" variant="brand" />
-          <Input placeholder="Search" size="sm" variant="brand" />
-        </HStack>
+        <VStack align="start" spacing="1rem">
+          <Heading as="h2" size="h2">
+            Input
+          </Heading>
+          <HStack>
+            <Input placeholder="Search" size="md" variant="brand" />
+            <Input placeholder="Search" size="sm" variant="brand" />
+          </HStack>
+          <HStack>
+            <NumberInput maxW="54px" size="md" />
+            <NumberInput maxW="38px" size="sm" />
+          </HStack>
+        </VStack>
       </VStack>
     </Container>
   );
