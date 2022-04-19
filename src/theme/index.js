7b941602fb52a574a6ee37f57d5ff1db56a3cc29
AlexandrLo <alexandr.lo@outlook.com>
2022-04-19 20:35:58 +0300

    Add global styles

--- src/theme/index.js
+++ src/theme/index.js
@@ -1,4 +1,5 @@
 import { extendTheme } from "@chakra-ui/react";
+import { mode } from "@chakra-ui/theme-tools";
 
 import "@fontsource/poppins/latin.css";
 
@@ -12,6 +13,25 @@ const theme = extendTheme({
   config: {
     initialColorMode: "system",
     useSystemColorMode: false,
+  },
+  styles: {
+    global: (props) => ({
+      body: {
+        fontFamily: "body",
+        color: mode("black", "white")(props),
+        bg: mode("white", "gray.800")(props),
+        transitionProperty: "background-color",
+        transitionDuration: "normal",
+        lineHeight: "base",
+      },
+      "*::placeholder": {
+        color: mode("gray.400", "whiteAlpha.400")(props),
+      },
+      "*, *::before, &::after": {
+        borderColor: mode("gray.200", "whiteAlpha.300")(props),
+        wordWrap: "break-word",
+      },
+    }),
   },
   fonts: {
     heading: "Poppins",
