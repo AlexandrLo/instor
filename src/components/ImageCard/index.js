6b465a94519f1e40d31defe771bd160bd63eb431
AlexandrLo <alexandr.lo@outlook.com>
2022-04-20 23:49:03 +0300

    Add ImageCard

---
+++ src/components/ImageCard/index.js
@@ -0,0 +1,64 @@
+import React from "react";
+
+import { NavLink } from "react-router-dom";
+import PropTypes from "prop-types";
+import { Image, Link, Text } from "@chakra-ui/react";
+
+function ImageCard({
+  name,
+  image,
+  to,
+  isDragging = false,
+  textProps = {},
+  ...props
+}) {
+  return (
+    <Link
+      as={NavLink}
+      to={to}
+      position="relative"
+      w="100%"
+      draggable={false}
+      onClick={(e) => {
+        // Disable onClick while dragging
+        if (isDragging) {
+          e.stopPropagation();
+          e.preventDefault();
+        }
+      }}
+      onDragStart={(e) => {
+        // Hack for firefox
+        e.stopPropagation();
+        e.preventDefault();
+      }}
+      {...props}
+    >
+      <Image
+        boxSize="100%"
+        src={image}
+        alt="Card image"
+        fit="cover"
+        borderRadius="1rem"
+        draggable={false}
+      />
+      <Text
+        position="absolute"
+        fontWeight="600"
+        draggable={false}
+        {...textProps}
+      >
+        {name}
+      </Text>
+    </Link>
+  );
+}
+
+ImageCard.propTypes = {
+  name: PropTypes.string.isRequired,
+  image: PropTypes.string.isRequired,
+  to: PropTypes.string.isRequired,
+  isDragging: PropTypes.bool,
+  textProps: PropTypes.object,
+};
+
+export default ImageCard;
