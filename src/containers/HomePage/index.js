d0a572dd850f1a7e09c7037888759ceade8c9a2e
AlexandrLo <alexandr.lo@outlook.com>
2022-04-22 22:43:11 +0300

    Update HomePage

--- src/containers/HomePage/index.js
+++ src/containers/HomePage/index.js
@@ -1,7 +1,27 @@
 import React from "react";
 
+import Banner from "components/Banner";
+import CategoriesBlock from "components/CategoriesBlock";
+import Footer from "components/Footer";
+import Hero from "components/Hero";
+import NavBar from "components/NavBar";
+import PopularBlock from "components/PopularBlock";
+import RoomsBlock from "components/RoomsBlock";
+import SlidingBanner from "components/SlidingBanner";
+
 function HomePage() {
-  return <>Home</>;
+  return (
+    <>
+      <NavBar />
+      <Hero />
+      <SlidingBanner />
+      <CategoriesBlock />
+      <PopularBlock />
+      <Banner />
+      <RoomsBlock />
+      <Footer />
+    </>
+  );
 }
 
 export default HomePage;
