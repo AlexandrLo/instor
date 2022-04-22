663e1eaf68c9056bfa66a20e6d4190c9c9a60d65
AlexandrLo <alexandr.lo@outlook.com>
2022-04-22 22:31:54 +0300

    Update Banner

--- src/components/Banner/index.js
+++ src/components/Banner/index.js
@@ -1,41 +1,41 @@
 import React from "react";
 
 import { NavLink } from "react-router-dom";
-import PropTypes from "prop-types";
-import { Image, Link } from "@chakra-ui/react";
+import { Container, Image, Link } from "@chakra-ui/react";
 
 import BannerOverlay from "components/BannerOverlay";
 
-function Banner({ data, ...props }) {
+const bannerData = {
+  image: "assets/banner/banner-2.jpg",
+  heading: "Sale",
+  lead: "All chairs up to 50% off",
+  to: "/",
+};
+
+function Banner(props) {
   return (
-    <Link
-      as={NavLink}
-      to={data.to}
-      w="100%"
-      display="block"
-      position="relative"
-      {...props}
-    >
-      <Image
-        src={data.image}
-        alt="Banner image"
-        h={{ base: "120px", md: "250px" }}
+    <Container maxW="container.xl">
+      <Link
+        variant="box"
+        as={NavLink}
+        to={bannerData.to}
         w="100%"
-        fit="cover"
-        borderRadius="1rem"
-      />
-      <BannerOverlay heading={data.heading} lead={data.lead} />
-    </Link>
+        display="block"
+        position="relative"
+        {...props}
+      >
+        <Image
+          src={bannerData.image}
+          alt="Banner image"
+          h={{ base: "120px", md: "250px" }}
+          w="100%"
+          fit="cover"
+          borderRadius="1rem"
+        />
+        <BannerOverlay heading={bannerData.heading} lead={bannerData.lead} />
+      </Link>
+    </Container>
   );
 }
-
-Banner.propTypes = {
-  data: PropTypes.shape({
-    image: PropTypes.string,
-    heading: PropTypes.string,
-    lead: PropTypes.string,
-    to: PropTypes.string,
-  }),
-};
 
 export default Banner;
