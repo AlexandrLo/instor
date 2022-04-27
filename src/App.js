3c180bcf3393fddc0e4958ea1e47fa036ebc0363
Alexandr <alexandr.lo@outlook.com>
2022-04-27 23:15:22 +0300

    Remove test page

--- src/App.js
+++ src/App.js
@@ -4,13 +4,11 @@ import { Route, Routes } from "react-router-dom";
 import CartPage from "containers/CartPage";
 import HomePage from "containers/HomePage";
 import PageLayout from "components/PageLayout";
-import TestPage from "containers/TestPage";
 
 export default function App() {
   return (
     <PageLayout>
       <Routes>
-        <Route path="/test" element={<TestPage />} />
         <Route path="" element={<HomePage />} />
         <Route path="/cart" element={<CartPage />} />
       </Routes>
