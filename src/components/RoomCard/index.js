0ffdbfecac608b4b1e8359d21ae53bd2c3a48d6b
Alexandr <alexandr.lo@outlook.com>
2022-04-28 21:25:38 +0300

    Update RoomCard

--- src/components/RoomCard/index.js
+++ src/components/RoomCard/index.js
@@ -1,22 +1,32 @@
 import React from "react";
 
+import PropTypes from "prop-types";
+
 import ImageCard from "components/ImageCard";
 
-function RoomCard(props) {
+function RoomCard({ name, image, to, isDragging, ...props }) {
   return (
     <ImageCard
       minW="180px"
       h={["245px", "260px", "275px", "290px", "300px"]}
       textProps={{
-        left: "0",
-        top: "0",
-        m: "1.5rem",
-        maxW: "3.75rem",
         color: "black",
+        maxW: "3.75rem",
       }}
+      name={name}
+      image={image}
+      to={to}
+      isDragging={isDragging}
       {...props}
     />
   );
 }
+
+RoomCard.propTypes = {
+  name: PropTypes.string.isRequired,
+  image: PropTypes.string.isRequired,
+  to: PropTypes.string.isRequired,
+  isDragging: PropTypes.bool,
+};
 
 export default RoomCard;
