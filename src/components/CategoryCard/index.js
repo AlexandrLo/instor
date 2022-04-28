f6dc46212ad019027e3c7a132a6196f6b730d0bd
Alexandr <alexandr.lo@outlook.com>
2022-04-28 21:24:28 +0300

    Update CateforyCard

--- src/components/CategoryCard/index.js
+++ src/components/CategoryCard/index.js
@@ -1,23 +1,34 @@
 import React from "react";
 
+import PropTypes from "prop-types";
+
 import ImageCard from "components/ImageCard";
 
-function CategoryCard(props) {
+function CategoryCard({ name, image, to, isDragging, ...props }) {
   return (
     <ImageCard
       minW="150px"
       h={["75px", "75px", "95px", "115px", "135px", "160px"]}
       textProps={{
         color: "white",
-        left: "0",
         top: "50%",
         transform: "auto",
         translateY: "-50%",
-        pl: { base: "1rem", md: "1.5rem" },
       }}
+      name={name}
+      image={image}
+      to={to}
+      isDragging={isDragging}
       {...props}
     />
   );
 }
+
+CategoryCard.propTypes = {
+  name: PropTypes.string.isRequired,
+  image: PropTypes.string.isRequired,
+  to: PropTypes.string.isRequired,
+  isDragging: PropTypes.bool,
+};
 
 export default CategoryCard;
