14d872057758ef13512c5c03bd9b0c20c4288ffc
Alexandr <alexandr.lo@outlook.com>
2022-05-02 23:24:24 +0300

    Add Price

---
+++ src/components/Price/index.js
@@ -0,0 +1,55 @@
+import React from "react";
+
+import PropTypes from "prop-types";
+import { HStack, Heading, Text } from "@chakra-ui/react";
+
+function Price({ price, discount, size = "md" }) {
+  if (size === "lg") {
+    return (
+      <HStack spacing="0.5rem">
+        {discount ? (
+          <>
+            <Heading
+              size="h2"
+              fontWeight="400"
+              textDecoration="line-through"
+              color="gray.400"
+            >
+              ${price}
+            </Heading>
+            <Heading size="h2" color="red.400">
+              ${discount}
+            </Heading>
+          </>
+        ) : (
+          <Heading size="h2">${price}</Heading>
+        )}
+      </HStack>
+    );
+  }
+
+  return (
+    <HStack spacing="0.25rem">
+      {discount ? (
+        <>
+          <Text fontWeight="400" textDecoration="line-through" color="gray.400">
+            ${price}
+          </Text>
+          <Text fontWeight="600" color="red.400">
+            ${discount}
+          </Text>
+        </>
+      ) : (
+        <Text fontWeight="600">${price}</Text>
+      )}
+    </HStack>
+  );
+}
+
+Price.propTypes = {
+  price: PropTypes.number.isRequired,
+  discount: PropTypes.number,
+  size: PropTypes.oneOf(["md", "lg"]),
+};
+
+export default Price;
