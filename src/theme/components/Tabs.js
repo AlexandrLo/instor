272a52e1d6ef3278e5e391c0ac3f8741b55afc6f
Alexandr <alexandr.lo@outlook.com>
2022-05-05 19:15:25 +0300

    Update theme

--- src/theme/components/Tabs.js
+++ src/theme/components/Tabs.js
@@ -1,11 +1,17 @@
 import { transparentize } from "@chakra-ui/theme-tools";
 
 export default {
+  baseStyle: {
+    tabpanel: {
+      p: { base: "1rem", md: "1.5rem" },
+    },
+  },
   variants: {
     "soft-rounded": (props) => {
       const { colorScheme: c, theme } = props;
       return {
         tab: {
+          whiteSpace: "nowrap",
           borderRadius: "full",
           fontWeight: "semibold",
           color: "gray.400",
