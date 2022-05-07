d6ba80f7aa734f110473e9216cee9ccac7a8276e
Alexandr <alexandr.lo@outlook.com>
2022-05-07 19:09:38 +0300

    Update SearchInput

--- src/components/SearchInput/index.js
+++ src/components/SearchInput/index.js
@@ -1,8 +1,14 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 
-import { ArrowRight24Filled, Search24Filled } from "@fluentui/react-icons";
+import { useNavigate } from "react-router-dom";
+import {
+  ArrowRight24Filled,
+  Dismiss16Filled,
+  Search24Filled,
+} from "@fluentui/react-icons";
 import {
   Button,
+  HStack,
   Input,
   InputGroup,
   InputLeftElement,
@@ -14,26 +20,56 @@ function SearchInput() {
   const iconColor = useColorModeValue("blackAlpha.500", "whiteAlpha.500");
   const [query, setQuery] = useState("");
 
+  const ref = useRef(null);
+
+  let navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    navigate(`/search?query=${query}`);
+  };
+
+  const clearInput = () => {
+    setQuery("");
+    ref.current.focus();
+  };
+
   return (
-    <InputGroup>
-      <InputLeftElement pointerEvents="none" color={iconColor}>
-        <Search24Filled />
-      </InputLeftElement>
-      <Input
-        placeholder="What are you looking for?"
-        value={query}
-        onChange={(e) => {
-          setQuery(e.target.value);
-        }}
-      />
-      {query && (
-        <InputRightElement color={iconColor}>
-          <Button size="sm">
-            <ArrowRight24Filled />
-          </Button>
-        </InputRightElement>
-      )}
-    </InputGroup>
+    <form onSubmit={handleSubmit}>
+      <InputGroup>
+        <InputLeftElement pointerEvents="none" color={iconColor}>
+          <Search24Filled />
+        </InputLeftElement>
+        <Input
+          ref={ref}
+          placeholder="What are you looking for?"
+          value={query}
+          pr={query && "5.25rem"}
+          onChange={(e) => {
+            setQuery(e.target.value);
+          }}
+          onSubmit={handleSubmit}
+        />
+
+        {query && (
+          <InputRightElement width="5.25rem" color={iconColor}>
+            <HStack spacing="0.25rem">
+              <Button
+                size="sm"
+                variant="ghost"
+                colorScheme="orange"
+                onClick={clearInput}
+              >
+                <Dismiss16Filled />
+              </Button>
+              <Button type="submit" size="sm">
+                <ArrowRight24Filled />
+              </Button>
+            </HStack>
+          </InputRightElement>
+        )}
+      </InputGroup>
+    </form>
   );
 }
 
