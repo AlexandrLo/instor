4eb33647693a4311a2e4e85bf85a7ba0c5483c82
Alexandr <alexandr.lo@outlook.com>
2022-05-19 16:12:26 +0300

    Use react-helmet-async

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 
 import "focus-visible/dist/focus-visible";
 import { HashRouter } from "react-router-dom";
+import { HelmetProvider } from "react-helmet-async";
 import { PersistGate } from "redux-persist/integration/react";
 import { Provider as ReduxProvider } from "react-redux";
 import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
@@ -16,15 +17,19 @@ ReactDOM.render(
   <React.StrictMode>
     <ReduxProvider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <ChakraProvider
-          theme={theme}
-          colorModeManager={customLocalStorageManager}
-        >
-          <HashRouter>
-            <ColorModeScript initialColorMode={theme.config.initialColorMode} />
-            <App />
-          </HashRouter>
-        </ChakraProvider>
+        <HelmetProvider>
+          <ChakraProvider
+            theme={theme}
+            colorModeManager={customLocalStorageManager}
+          >
+            <HashRouter>
+              <ColorModeScript
+                initialColorMode={theme.config.initialColorMode}
+              />
+              <App />
+            </HashRouter>
+          </ChakraProvider>
+        </HelmetProvider>
       </PersistGate>
     </ReduxProvider>
   </React.StrictMode>,
